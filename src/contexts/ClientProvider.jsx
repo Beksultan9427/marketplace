5ca6674b202f18f882230193474a5d38dc4f1d84
Beksultan <beksultan9427@gmail.com>
2022-07-28 21:03:57 +0600

    Выполнено: pagination, начало Корзины

--- src/contexts/ClientProvider.jsx
+++ src/contexts/ClientProvider.jsx
@@ -21,12 +21,25 @@ function ClientProvider({ children }) {
   const [searchWord, setSearchWord] = useState("");
   const [filterByPrice, setFilterByPrice] = useState([0, 5000]);
 
+  const limit = 2;
+  const [pagesCount, setPagesCount] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+
   const getWatches = () => {
     fetch(
-      `${watchesApi}?q=${searchWord}&price_gte=${filterByPrice[0]}&price_lte${filterByPrice[1]}`
+      `${watchesApi}?q=${searchWord}&price_gte=${filterByPrice[0]}&price_lte${filterByPrice[1]}&_limit=${limit}&_page=${currentPage}`
     )
-      .then((res) => res.json())
+      // ! res - делает запрос на сервер => (data)
+      .then((res) => {
+        let count = Math.ceil(res.headers.get("X-Total-Count") / limit);
+        setPagesCount(count);
+        return res.json();
+      })
+      // ! data - backend(база данных)
       .then((data) => {
+        // ! Math.ceil() - округляет вверх (1.2 = 2, 1.1 = 2)
+        // ! Math.floor() - окргуляет вниз (1.9 = 1, 1.1 = 1)
+        // ! Math.round() - округляет как в матиматике (1.1 = 1, 1.6 = 2)
         let action = {
           type: "GET_WATCHES",
           payload: data,
@@ -35,13 +48,42 @@ function ClientProvider({ children }) {
       });
   };
 
+  const addWatchToBasket = (watch) => {
+    // ! localStorage.setItem() - добавление
+    // ! localStorage.getItem() -
+    // ! localStorage.removeItem() -
+    // ! localStorage.clear() - очитска всего
+    let basket = JSON.parse(localStorage.getItem("basket"));
+    if (!basket) {
+      basket = {
+        totalPrice: 0,
+        products: [],
+      };
+    }
+
+    let watchToBasket = {
+      ...watch,
+      count: 1,
+      subPrice: watch.price,
+    };
+    basket.products.push(watchToBasket);
+    basket.totalPrice = basket.products.reduce((prev, item) => {
+      return prev + item.subPrice;
+    }, 0);
+    console.log(basket);
+  };
+
   const data = {
     watches: state.watches,
     searchWord,
     filterByPrice,
+    pagesCount,
+    currentPage,
     getWatches,
     setSearchWord,
     setFilterByPrice,
+    setCurrentPage,
+    addWatchToBasket,
   };
   return (
     <ClientContext.Provider value={data}>{children}</ClientContext.Provider>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -8,16 +8,25 @@ import {
   Button,
   Typography,
   Slider,
+  Pagination,
 } from "@mui/material";
 import { ClientContext } from "../contexts/ClientProvider";
 
 function MainPage() {
-  const { getWatches, watches, filterByPrice, setFilterByPrice } =
-    useContext(ClientContext);
+  const {
+    getWatches,
+    watches,
+    filterByPrice,
+    setFilterByPrice,
+    pagesCount,
+    setCurrentPage,
+    currentPage,
+    addWatchToBasket,
+  } = useContext(ClientContext);
 
   useEffect(() => {
     getWatches();
-  }, [filterByPrice]);
+  }, [filterByPrice, currentPage]);
 
   return (
     <div className="main-page">
@@ -64,9 +73,22 @@ function MainPage() {
                     <span>{item.price} $</span>
                   </li>
                 </ul>
+                <Button
+                  onClick={() => addWatchToBasket(item)}
+                  variant="outlined"
+                >
+                  Добавить в корзину
+                </Button>
               </CardContent>
             </Card>
           ))}
+        </div>
+        <div className="pagination-block">
+          <Pagination
+            onChange={(_, newValue) => setCurrentPage(newValue)}
+            count={pagesCount}
+            shape="rounded"
+          />
         </div>
       </Container>
     </div>
